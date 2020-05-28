a2e8fa7442071d8de20d585897b6356f23d5d29c
Denis Dolgih <densan32@mail.ru>
2020-05-28 22:35:03 +0300

    просто пробывал ветку

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+if($_POST["vozrast"]<=80)
+    echo "Успехов";
+else
+    echo"Здравствуйте,Уважаемый!";
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Обработка форм</title>
+
+</head>
+<body>
+    <form name="test" action="" method="post">
+        <label>Имя:</label><br />
+        <input type="text" name="name" placeholder="имя"/><br />
+        <label>Возраст:</label><br />
+        <input type="text" name="vozrast" placeholder="Возраст"/><br />
+        <input type="submit" name="done" value="Готово"/>
+    </form>
+</body>
+</html>
