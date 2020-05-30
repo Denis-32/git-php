6a07259b97234f90d324c029a51822efadf19545
Denis Dolgih <densan32@mail.ru>
2020-05-30 10:24:33 +0300

    моя вторая форма страны

--- index.php
+++ index.php
@@ -1,24 +1,65 @@
-<?php
-if($_POST["vozrast"]<=80)
-    echo "Успехов";
-else
-    echo"Здравствуйте,Уважаемый!";
-
-?>
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
 <head>
-    <title>Обработка форм</title>
-
+    <meta charset="utf-8">
+    <title>Тег SELECT</title>
 </head>
 <body>
-<form name="test" action="" method="post">
-    <label>Имя:</label><br />
-    <input type="text" name="name" placeholder="имя"/><br />
-    <label>Возраст:</label><br />
-    <input type="text" name="vozrast" placeholder="Возраст"/><br />
-    <input type="submit" name="done" value="Готово"/>
+
+<form action="" method="post">
+    <p><select name="hero">
+            <option label="Турция" value="1" selected>Турция</option>
+            <option value="2">Египет</option>
+            <option value="3">Италия</option>
+        </select>
+    <p><input type="submit" value="Отправить"></p>
 </form>
 </body>
-
 </html>
+<?php
+echo '<pre>';
+
+print_r($_POST);
+
+echo '</pre>';
+if (isset($_POST['hero']))
+{
+    if($_POST['hero'] == 1)
+    {
+
+
+        if(isset($_POST['b']))
+
+
+        {
+
+            echo" в Турция со скидкой ". ($_POST['a']*400*1.12*0.95);}
+
+
+        else{echo" в Турция без скидки ". ($_POST['a']*400*1.12);}
+
+
+    }
+    elseif( $_POST['hero'] == 2){
+
+        if(isset($_POST['b'])){
+
+            echo" в Египет со скидкой". ($_POST['a']*400*1.1*0.95);}
+
+
+        else{echo"Египет без скидкии".($_POST['a']*400*1.1);}
+
+    }
+
+    elseif($_POST['hero'] == 3){
+
+        if(isset($_POST['b'])){
+
+            echo" в Италия со скидкой". ($_POST['a']*400*0.95);}
+
+        else{echo"Италия без скидкии".($_POST['a']*400);}
+    }
+}
+?>
+
+?>
