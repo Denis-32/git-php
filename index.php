24e74ebf6156e0e6c7d727dfa6085d952eaa4ae2
Denis Dolgih <densan32@mail.ru>
2020-05-30 08:46:15 +0300

    моя первая форма возраст

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
-$x=9;
-$y=4;
-$c=(pow($x, 2)-4*sqrt($y-1))/2+$x;
-echo"$c<br />";
+if($_POST["vozrast"]<=80)
+    echo "Успехов";
+else
+    echo"Здравствуйте,Уважаемый!";
+
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Обработка форм</title>
+
+</head>
+<body>
+<form name="test" action="" method="post">
+    <label>Имя:</label><br />
+    <input type="text" name="name" placeholder="имя"/><br />
+    <label>Возраст:</label><br />
+    <input type="text" name="vozrast" placeholder="Возраст"/><br />
+    <input type="submit" name="done" value="Готово"/>
+</form>
+</body>
+</html>
