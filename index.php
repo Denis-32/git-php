0e837f549fbc0186e00fb372383a9ce0eb49f16f
Denis Dolgih <densan32@mail.ru>
2020-05-31 09:32:35 +0300

    мой второй цикл таблица цветов

--- index.php
+++ index.php
@@ -1,35 +1,18 @@
 <?php
-if(isset($_POST['done'])) {
-    if (($_POST['a']==''))
-        echo "Введите все поля ";
-    elseif(($_POST['b']==''))
-        echo "Введите все поля ";
-    elseif(($_POST['a']>$_POST['b']))
-        echo "Суммы не существует";
-    else {
-        while ($_POST['a'] <= $_POST['b']) {
-            $iSum = $iSum + $_POST['a'];
-            $_POST['a']++;
+echo '<table border=1>';
+for ($i=100; $i<=500; $i +=100)
+{
+    echo '<tr>';
+
+    for ($j=250; $j<=500; $j +=200 )
+    {
+        for ($k=300; $k<=600; $k += 100)
+        {
+            echo '<td style="background-color:RGB('.$i.', '.$j.', '.$k.');"> rgb('.$i.', '.$j.', '.$k.')</td>';
         }
-        echo "Равно=$iSum";
     }
+
+    echo '</tr>';
 }
+echo'</table>';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-<form action="" method="post">
-    <p>от</p>
-        <label>A</label><br />
-        <input type="text" name="a" placeholder="число"/><br />
-    <p>до</p>
-        <label>Б</label><br />
-        <input type="text" name="b" placeholder="число"/><br />
-    <p><input type="submit" name="done" value="сумма"></p>
-</form>
-</body>
-</html>
